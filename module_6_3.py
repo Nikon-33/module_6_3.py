b55ab10714e064cf5764929ba265cff2a16e934c
Nikon-33 <89101717800@mail.ru>
2024-07-16 18:41:09 +0300

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -21,8 +21,9 @@ class Eagle:
 
 class Pegasus(Horse, Eagle):
     def __init__(self):
-        Horse.__init__(self)
+        super().__init__()
         Eagle.__init__(self)
+
 
     def move(self, dx, dy):
         self.run(dx)
